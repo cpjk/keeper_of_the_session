91e98438bb178e7c6ecbdb8361994c461b4b22a7
Chris Kelly <cpjkelly@gmail.com>
2016-10-31 14:07:19 -0700

    Make interval more clear

--- keep_session.js
+++ keep_session.js
@@ -1,9 +1,11 @@
 console.log("Keeping the session alive by fetching the page every 5 minutes.");
 
+let INTERVAL_MINS = 5;
+
 // async fetch the current path every 5 minutes
 window.setInterval(()=> {
   let xhr = new XMLHttpRequest();
   xhr.open("GET", window.location.href, true); // async fetch the current page
   xhr.send(null); // send the request
-}, 300000);
+}, INTERVAL_MINS * 60 * 1000);
 
